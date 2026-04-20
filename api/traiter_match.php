e957a7382cbf909537fd304daa66835a77248ead
Azeddine <azeddinazhari@gmail.com>
2026-04-20 12:57:05 +0200

    Add DB_PORT support for remote MySQL hosting

--- api/traiter_match.php
+++ api/traiter_match.php
@@ -10,8 +10,9 @@ $db_host = getenv('DB_HOST') ?: "localhost";
 $db_user = getenv('DB_USER') ?: "root";
 $db_pass = getenv('DB_PASS') ?: "";
 $db_name = getenv('DB_NAME') ?: "koranow_db";
+$db_port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 // Vérifier connexion
 if ($conn->connect_error) {
